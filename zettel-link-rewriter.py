f43aa364354c78403ee83de4296541824adead72
Balaji Dutt <me@balaji-dutt.name>
2020-06-07 16:58:31 +0800

    Rewrite check_dirs() to use global vars.

--- zettel-link-rewriter.py
+++ zettel-link-rewriter.py
@@ -5,23 +5,11 @@ import logging
 import pathlib
 import configargparse
 
-# Defining variables
-src_dir = pathlib.Path("F:/Balaji/Development/zettel-link-rewriter/source")
-target_dir = pathlib.Path("F:/Balaji/Development/zettel-link-rewriter/target")
-
 # Configure logging early
 logging.basicConfig(level=10,  # DEBUG by default
                     format="%(asctime)s %(levelname)-8s %(funcName)s():%(lineno)i:        %(message)s",
                     datefmt="%Y-%m-%d %H:%M:%S")
 
-
-# def someFunc(myList = [], *args):
-#     for x in myList:
-#         print(x)
-#
-# items = [1,2,3,4,5]
-#
-# someFunc(items)
 
 def parse_config():
     default_config = pathlib.PurePath(__file__).stem + ".ini"
@@ -38,10 +26,10 @@ def parse_config():
                         help='Write log messages to a file', metavar='LOGFILE')
     config.add_argument('--source_files', action='store',
                         help='Specify path to directory containing source markdown files. Default is current directory.'
-                        , default=pathlib.PurePath(__file__).parent)
+                        , default=pathlib.PurePath(__file__).parent, metavar="DIRECTORY")
     config.add_argument('--target_files', action='store',
                         help='Specify path to directory containing source markdown files. Default is current directory.'
-                        , default=pathlib.Path.joinpath(pathlib.PurePath(__file__).parent, "dest"))
+                        , default=pathlib.Path.joinpath(pathlib.PurePath(__file__).parent, "dest"), metavar="DIRECTORY")
     options = config.parse_known_args()
     # Convert tuple of parsed arguments into a dictionary. There are two values within this tuple.
     # [0] represents recognized arguments. [1] represents unrecognized arguments on command-line or config file.
@@ -60,18 +48,6 @@ def parse_config():
     logger = logging.getLogger()
     logger.setLevel(logging_level.upper())
 
-    # Check if specified config file exists else bail
-    try:
-        pathlib.Path(config_file).exists()
-    except:
-        logging.exception('Did not find the specified configuration file %s', config_file)
-        raise FileNotFoundError
-    else:
-        if config_file == default_config:
-            logging.debug("Using the default configuration file %s", default_config)
-        else:
-            logging.debug("Found configuration file %s", config_file)
-
     # Configure file-based logging
     if log_file is None:
         logging.debug("No log file set. All log messages will print to Console only")
@@ -84,20 +60,36 @@ def parse_config():
         logger.addHandler(filelogger)
         logging.warning("Outputting to log file")
 
-
-def check_dir(*args):
-    for item in args:
-        # print(item)
-        if os.path.isdir(item):
-            pass
+    # Check if specified config file exists else bail
+    try:
+        pathlib.Path(config_file).exists()
+    except:
+        logging.exception('Did not find the specified configuration file %s', config_file)
+        raise FileNotFoundError
+    else:
+        if config_file == default_config:
+            logging.debug("Using the default configuration file %s", default_config)
         else:
-            logging.exception('Did not find the directory %s', item)
-            raise NotADirectoryError
+            logging.debug("Found configuration file %s", config_file)
+
+
+def check_dirs(source_dir, target_dir):
+    if pathlib.Path(source_dir).exists():
+        pass
+    else:
+        logging.exception('Did not find the directory %s', source_dir)
+        raise NotADirectoryError
+
+    if pathlib.Path(target_dir).exists():
+        pass
+    else:
+        logging.warning('Did not find the target directory %s. Will try create it now', target_dir)
+        pathlib.Path(target_dir).mkdir(exist_ok=True)
+
 
 def main():
     parse_config()
-    # dir_list = [src_dir, target_dir]
-    # check_dir(*dir_list)
+    check_dirs(source_files, target_files)
 
 
 if __name__ == '__main__':
